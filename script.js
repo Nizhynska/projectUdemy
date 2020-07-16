e87d5af082f1c82bea5f1a1e720d92a296a69e4b
polina <polinanizhynska@gmail.com>
2020-07-17 01:42:29 +0300

    finish task_3

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ function questions () {
     while(count != 3) {
         let lastMovie = prompt("One of the las movie you had seen", "London field");
         let movieRate = prompt("Please, ate the movie", "10");
+        persolanMovieDB.movies[lastMovie] = movieRate;
+        persolanMovieDB.movies[lastMovie] = movieRate;
         count++;
     }
 }
 questions();
+
+console.log(persolanMovieDB);
