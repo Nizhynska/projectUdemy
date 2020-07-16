d9665f821baff281579c6a363c88808d97514f61
polina <polinanizhynska@gmail.com>
2020-07-17 01:10:20 +0300

    add object task_2

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 let numberOfFilms = prompt("how many movies hav you watched", "10");
+let persolanMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
