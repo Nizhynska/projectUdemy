99924e411222fd97189c43dcd4f7b664503ac10a
polina <polinanizhynska@gmail.com>
2020-07-17 01:19:06 +0300

    add questions task_3

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ let persolanMovieDB = {
     genres: [],
     private: false
 };
+
+function questions () {
+    count = 1;
+    while(count != 3) {
+        let lastMovie = prompt("One of the las movie you had seen", "London field");
+        let movieRate = prompt("Please, ate the movie", "10");
+        count++;
+    }
+}
+questions();
